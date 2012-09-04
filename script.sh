4d929315a67f6e6075a2a35428beef641ebe3379
Abdul Azeez <abdulazeez1984@gmail.com>
2012-09-04 22:31:59 +0530

    Changed script.sh file to generic

--- script.sh
+++ script.sh
@@ -1,6 +1,6 @@
-server="Server"
-
-cd $server
+export NEO4J_REST_URL=http://localhost:7474/db/data
+export NEO4J_LOGIN=""
+export NEO4J_PASSWORD=""
 echo " ************** Running Maven install ********* "
 mvn clean install
 
