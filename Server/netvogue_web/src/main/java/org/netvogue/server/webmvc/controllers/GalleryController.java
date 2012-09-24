c7467475f77b33897ce803c10b9e1b147db06eca
Abdul Azeez <abdulazeez1984@gmail.com>
2012-09-25 01:19:38 +0530

    Added file uploading functionality for Add gallery page

---
+++ Server/netvogue_web/src/main/java/org/netvogue/server/webmvc/controllers/GalleryController.java
@@ -0,0 +1,62 @@
+package org.netvogue.server.webmvc.controllers;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+import org.netvogue.server.aws.core.ImageType;
+import org.netvogue.server.aws.core.UploadManager;
+import org.netvogue.server.neo4japi.domain.User;
+import org.netvogue.server.webmvc.domain.UploadedFile;
+import org.netvogue.server.webmvc.security.NetvogueUserDetailsService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
+
+@Controller
+@RequestMapping("AddPhotos")
+public class GalleryController {
+
+	@Autowired NetvogueUserDetailsService userDetailsService;
+
+	@Autowired
+	private UploadManager uploadManager;
+	
+	/*@RequestMapping(method=RequestMethod.GET)
+	public ModelAndView ShowAddCollections(Model model) throws Exception {
+		System.out.println("Comming to Add Collections");
+		final User user = userDetailsService.getUserFromSession();
+		return new ModelAndView("AddCollections2", "Collections", new Collections());
+
+	}*/
+	
+	@RequestMapping(method=RequestMethod.POST)
+	public @ResponseBody List<UploadedFile> AddPhotostoGallery(Model model, @RequestParam("files[]") List<MultipartFile> fileuploads) {
+		System.out.println("Add photos");
+		final User user = userDetailsService.getUserFromSession();
+		List<UploadedFile> JSONFileData= new ArrayList<UploadedFile>();
+		
+		for ( MultipartFile fileupload : fileuploads ) {
+			System.out.println("Came here" + fileupload.getOriginalFilename());
+			Map<String, Object> uploadMap  = uploadManager.processUpload(fileupload, ImageType.GALLERY);
+			String imagePath = (String)uploadMap.get(UploadManager.QUERY_STRING);
+		
+			UploadedFile fileUploaded = new UploadedFile(fileupload.getOriginalFilename(),
+				Long.valueOf(fileupload.getSize()).intValue(), imagePath);
+			JSONFileData.add(fileUploaded);
+		}
+		/*if ( collection.getFileData() instanceof MultipartFile )
+			
+		else {
+			LinkedList<MultipartFile> list = (LinkedList<MultipartFile>)collection.getFileData();
+			uploadManager.processUpload(list, ImageType.COLLECTIONS);
+		}*/
+		return JSONFileData;
+	}
+
+}
