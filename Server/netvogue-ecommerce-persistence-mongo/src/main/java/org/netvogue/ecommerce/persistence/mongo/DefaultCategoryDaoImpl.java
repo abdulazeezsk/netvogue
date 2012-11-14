9030b1ad3c664e5a33de6cb9addb6af742987098
suman <psuman@prokarmasoftech.com>
2012-11-14 19:06:41 +0530

    Added mongo configuration

--- Server/netvogue-ecommerce-persistence-mongo/src/main/java/org/netvogue/ecommerce/persistence/mongo/DefaultCategoryDaoImpl.java
+++ Server/netvogue-ecommerce-persistence-mongo/src/main/java/org/netvogue/ecommerce/persistence/mongo/DefaultCategoryDaoImpl.java
@@ -2,10 +2,13 @@ package org.netvogue.ecommerce.persistence.mongo;
 
 import org.netvogue.ecommerce.domain.model.Category;
 import org.netvogue.ecommerce.persistence.CategoryDao;
+import org.springframework.data.mongodb.core.MongoTemplate;
 
 import java.util.Set;
 
 public class DefaultCategoryDaoImpl implements CategoryDao {
+
+  private MongoTemplate mongoTemplate;
 
   public void addCategory(final Category category) {
     // TODO Auto-generated method stub
@@ -27,4 +30,8 @@ public class DefaultCategoryDaoImpl implements CategoryDao {
     return null;
   }
 
+
+  public void setMongoTemplate(final MongoTemplate mongoTemplate) {
+    this.mongoTemplate = mongoTemplate;
+  }
 }
--- Server/netvogue-ecommerce-persistence-mongo/src/main/java/org/netvogue/ecommerce/persistence/mongo/DefaultReviewDaoImpl.java
+++ Server/netvogue-ecommerce-persistence-mongo/src/main/java/org/netvogue/ecommerce/persistence/mongo/DefaultReviewDaoImpl.java
@@ -3,8 +3,11 @@ package org.netvogue.ecommerce.persistence.mongo;
 import org.netvogue.ecommerce.domain.model.Review;
 import org.netvogue.ecommerce.domain.model.User;
 import org.netvogue.ecommerce.persistence.ReviewDao;
+import org.springframework.data.mongodb.core.MongoTemplate;
 
 public class DefaultReviewDaoImpl implements ReviewDao {
+
+  private MongoTemplate mongoTemplate;
 
   public void addReview(final long styleId, final Review review) {
     // TODO Auto-generated method stub
@@ -19,6 +22,10 @@ public class DefaultReviewDaoImpl implements ReviewDao {
   public void voteReview(final long reviewId, final long styleId, final User votedBy) {
     // TODO Auto-generated method stub
 
+  }
+
+  public void setMongoTemplate(final MongoTemplate mongoTemplate) {
+    this.mongoTemplate = mongoTemplate;
   }
 
 }
