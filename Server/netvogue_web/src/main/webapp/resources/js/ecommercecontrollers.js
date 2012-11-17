fa4659aab582b1b9d17676ef51ce464aeb8b0093
Shashank Ragireddy <shashank.ragireddy@gmail.com>
2012-11-18 01:59:16 +0530

    added myctrlinvite to commerce.js and show more alignment on profile page done

--- Server/netvogue_web/src/main/webapp/resources/js/ecommercecontrollers.js
+++ Server/netvogue_web/src/main/webapp/resources/js/ecommercecontrollers.js
@@ -5,3 +5,12 @@ function MyCtrlCart($scope, $routeParams, $location, currentvisitedprofile) {
 function MyCtrlOrders($scope, $routeParams, $location, currentvisitedprofile) {
 	
 }
+
+function MyCtrlInvite($scope, $routeParams, $location, currentvisitedprofile)
+{
+	$scope.$parent.title = 'Invite';
+//	$scope.entityname = currentvisitedprofile.getEntityName();
+//	$scope.isMyProfile = currentvisitedprofile.isMyProfile();
+//	$scope.backButton = currentvisitedprofile.getBackHistory();
+
+}
