f9bc2d8fe37f86a7a77e97a58f39f92665e2b942
suman <psuman@prokarmasoftech.com>
2012-11-26 17:34:29 +0530

    added net vogue authstore component

---
+++ Server/netvogue-ecommerce-persistence-mongo/src/main/java/org/netvogue/ecommerce/persistence/mongo/DefaultShoppingCartDaoImpl.java
@@ -0,0 +1,29 @@
+package org.netvogue.ecommerce.persistence.mongo;
+
+import org.netvogue.ecommerce.domain.model.CartItem;
+import org.netvogue.ecommerce.domain.model.ShoppingCart;
+import org.netvogue.ecommerce.persistence.ShoppingCartdao;
+
+public class DefaultShoppingCartDaoImpl implements ShoppingCartdao {
+
+  public ShoppingCart getCartForUser(final String userName) {
+    // TODO Auto-generated method stub
+    return null;
+  }
+
+  public void addItemToCart(final String userName, final CartItem item) {
+    // TODO Auto-generated method stub
+
+  }
+
+  public void deleteItemFromCart(final String userName, final CartItem item) {
+    // TODO Auto-generated method stub
+
+  }
+
+  public void updateCartItem(final String userName, final CartItem newItem) {
+    // TODO Auto-generated method stub
+
+  }
+
+}
