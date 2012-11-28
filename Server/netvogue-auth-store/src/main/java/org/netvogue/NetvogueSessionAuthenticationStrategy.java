436be604b76259b47d44340c72093156b9b6e43c
suman <psuman@prokarmasoftech.com>
2012-11-28 16:51:39 +0530

    Cutomized SessionAuthenticationStrategy to persiste authenticated principle in session.
    By default spring puts authenticated principal in security context only but not in session

---
+++ Server/netvogue-auth-store/src/main/java/org/netvogue/NetvogueSessionAuthenticationStrategy.java
@@ -0,0 +1,24 @@
+package org.netvogue;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.security.web.authentication.session.SessionAuthenticationException;
+import org.springframework.security.web.authentication.session.SessionAuthenticationStrategy;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+public class NetvogueSessionAuthenticationStrategy implements SessionAuthenticationStrategy {
+
+  public static String PRINCIPAL_KEY = "principal";
+  public static String PRICIPLE_ROLES_KEY = "principal_roles";
+
+  public void onAuthentication(final Authentication authentication, final HttpServletRequest request, final HttpServletResponse response)
+      throws SessionAuthenticationException {
+
+    HttpSession session = request.getSession();
+    session.setAttribute(PRINCIPAL_KEY, authentication.getPrincipal());
+    session.setAttribute(PRICIPLE_ROLES_KEY, authentication.getAuthorities());
+  }
+
+}
