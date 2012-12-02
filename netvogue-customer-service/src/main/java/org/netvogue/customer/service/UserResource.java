52a81aa2bcfe6d2fca4fde92bed78af2ae48fe8f
suman <psuman@prokarmasoftech.com>
2012-12-02 12:39:15 +0530

    Added few more place holder methods to UserResource

--- netvogue-customer-service/src/main/java/org/netvogue/customer/service/UserResource.java
+++ netvogue-customer-service/src/main/java/org/netvogue/customer/service/UserResource.java
@@ -9,8 +9,10 @@ import org.netvogue.ecommerce.domain.model.User;
 import org.netvogue.ecommerce.persistence.UserDao;
 
 import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.Response;
 
@@ -73,25 +75,137 @@ public class UserResource {
   }
 
 
-  @POST
-  @Path("find-details")
+  @GET
+  @Path("/${userName}/findDqetails")
   @Consumes("application/json")
   @Produces("application/json")
-  public Response findUserDetails(final String body) throws Exception {
+  public Response findUserDetails(@PathParam("userName") final String userName) throws Exception {
 
     ObjectMapper mapper = new ObjectMapper();
 
-    Map<String, String> map = mapper.readValue(new StringReader(body), new TypeReference<Map<String, String>>() {
-    });
-
-
-    UserRepresentation rep = userToUserRepconverter.convert(userDao.getActiveUser(map.get("userName")));
+    UserRepresentation rep = userToUserRepconverter.convert(userDao.getActiveUser(userName));
 
     StringWriter writer = new StringWriter();
 
     mapper.writeValue(writer, rep);
 
     return Response.ok(writer.toString()).build();
+  }
+
+  @POST
+  @Path("/${userName}/addLookBook")
+  @Consumes("application/json")
+  @Produces("application/json")
+  public Response addLookbook(@PathParam("userName") final String userName, final String lookbookDetails) {
+
+    return null;
+  }
+
+  @POST
+  @Path("/${userName}/addLineSheet")
+  @Consumes("application/json")
+  @Produces("application/json")
+ public Response addLinesheet(@PathParam("userName") final String userName, final String linesheetDetails) {
+
+    return null;
+ }
+
+  @POST
+  @Path("/${userName}/addStyleToLookbook/${lookbookId}")
+  @Consumes("application/json")
+  @Produces("application/json")
+  public Response addStyleToLookbook(@PathParam("userName") final String userName,
+                                     @PathParam("lookbookId") final String lookbookId,
+                                     final String styleDetails) {
+
+   return null;
+ }
+
+
+  @POST
+  @Path("/${userName}/addStyleToLinesheet/${linesheetId}")
+  @Consumes("application/json")
+  @Produces("application/json")
+ public Response addStyleToLinesheet(@PathParam("userName") final String userName,
+                                     @PathParam("linesheetId") final String linesheetId,
+                                     final String styleDetails) {
+   return null;
+ }
+
+  @POST
+  @Path("/${userName}/deleteStyle/${styleId}/FromLinesheet/${linesheetId}")
+  @Consumes("application/json")
+  @Produces("application/json")
+  public Response deleteStyleFromLinesheet(@PathParam("userName") final String userName,
+                                           @PathParam("styleId") final String styleId,
+                                           @PathParam("linesheetId") final String linesheetId) {
+   return null;
+  }
+
+  @POST
+  @Path("/${userName}/deleteStyle/${styleId}/FromLookbook/${lookbookId}")
+  @Consumes("application/json")
+  @Produces("application/json")
+  public Response deleteStyleFromLookbook(@PathParam("userName") final String userName,
+                                          @PathParam("styleId") final String styleId,
+                                          @PathParam("lookbookId") final String lookbookId) {
+   return null;
+  }
+
+
+  @POST
+  @Path("/${userName}/deleteLookbook/${lookbookId}")
+  @Consumes("application/json")
+  @Produces("application/json")
+  public Response deleteLookbook(@PathParam("userName") final String userName,
+                                 @PathParam("lookbookId") final String lookbookId) {
+   return null;
+  }
+
+
+  @POST
+  @Path("/${userName}/deleteLinesheet/${linesheetId}")
+  @Consumes("application/json")
+  @Produces("application/json")
+  public Response deleteLinesheet(@PathParam("userName") final String userName,
+                                  @PathParam("linesheetId") final String lookbookId) {
+   return null;
+  }
+
+  @GET
+  @Path("/${userName}/findLookBooks")
+  @Consumes("application/json")
+  @Produces("application/json")
+  public Response findLookBooksByUser(@PathParam("userName")  final String userName) {
+
+    return null;
+  }
+
+  @GET
+  @Path("/${userName}/findLinesheets")
+  @Consumes("application/json")
+  @Produces("application/json")
+  public Response findLinesheetsByUser(@PathParam("userName")  final String userName) {
+
+    return null;
+  }
+
+  @GET
+  @Path("/${userName}/findStylesByLinesheet/${linesheetId}")
+  @Consumes("application/json")
+  @Produces("application/json")
+  public Response findStylesByLinesheet(@PathParam("userName")  final String userName, @PathParam("linesheetId")  final String linesheetId) {
+
+    return null;
+  }
+
+  @GET
+  @Path("/${userName}/findStylesByLookbook/${linesheetId}")
+  @Consumes("application/json")
+  @Produces("application/json")
+  public Response findStylesByLookbook(@PathParam("userName")  final String userName, @PathParam("linesheetId")  final String linesheetId) {
+
+    return null;
   }
 
 
