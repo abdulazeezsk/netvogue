e7fa9d5d898fad375a7be196edae6d4509e54811
ptadikamalla <tpavan252@gmail.com>
2012-12-04 16:16:37 +0530

    Windows file for starting server

---
+++ Server/netvogue_web/script.bat
@@ -0,0 +1,9 @@
+call SET NEO4J_REST_URL=http://localhost:7474/db/data
+call SET NEO4J_LOGIN=""
+call SET NEO4J_PASSWORD=""
+call echo " ************** Running Maven install ********* "
+call mvn clean install
+
+call echo " ************** Running the build using webapp runner ********* "
+call SET env=local
+call java -jar target/netvogueweb.jar
