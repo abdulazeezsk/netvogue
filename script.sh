47507b32edf2a7c87e17091140544e1619a9f1c7
Shashank Ragireddy <shashank.ragireddy@gmail.com>
2013-03-22 23:59:25 +0530

    change in script.sh file

--- script.sh
+++ script.sh
@@ -8,5 +8,5 @@ export NEO4J_REST_URL=http://localhost:7474/db/data
 export NEO4J_LOGIN=""
 export NEO4J_PASSWORD=""
 export env=local
-kill $(ps -eo pid,command | grep "netvogueweb.jar" | grep -v grep | awk '{print $1}')
-java -jar ../Server/netvogue_web/target/netvogueweb.jar
+kill $(ps -eo pid,command | grep "netvogueweb.war" | grep -v grep | awk '{print $1}')
+java -jar ../Server/netvogue_web/target/dependency/webapp-runner.jar ../Server/netvogue_web/target/netvogueweb.war
